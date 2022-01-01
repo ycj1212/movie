433ce799a1b27e54260ac9b21b4df3d275c69613
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-01 22:39:11 +0900

    feature: MovieDetailsFragment 추가

---
+++ app/src/main/java/com/example/movie/MovieDetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.movie.databinding.FragmentMovieDetailsBinding
+
+class MovieDetailsFragment : Fragment() {
+    private lateinit var binding: FragmentMovieDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
