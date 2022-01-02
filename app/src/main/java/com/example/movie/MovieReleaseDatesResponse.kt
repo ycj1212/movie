531ac8ef10c37b8a5e9d3622704a2edbb8f442ee
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-02 15:23:36 +0900

    feature: API 응답 클래스 추가

---
+++ app/src/main/java/com/example/movie/MovieReleaseDatesResponse.kt
@@ -0,0 +1,19 @@
+package com.example.movie
+
+data class MovieReleaseDatesResponse(
+    val id: Int,
+    val results: List<MovieReleaseDatesResult>
+)
+
+data class MovieReleaseDatesResult(
+    val iso_3166_1: String,
+    val release_dates: List<ReleaseDate>
+)
+
+data class ReleaseDate(
+    val certification: String,
+    val iso_639_1: String,
+    val note: String,
+    val release_date: String,
+    val type: Int
+)
