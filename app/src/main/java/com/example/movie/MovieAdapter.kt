fad35183b3a400cd376ed2b129400eb9c5b6a10b
Yang Cheol Joo <rlaengks0001@naver.com>
2022-01-02 21:35:54 +0900

    feature: 뷰페이저 및 어댑터 설정

--- app/src/main/java/com/example/movie/MovieAdapter.kt
+++ app/src/main/java/com/example/movie/MovieAdapter.kt
@@ -1,12 +1,26 @@
 package com.example.movie
 
+import android.content.res.Configuration
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class MovieAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    private val fragmentList = mutableListOf<Fragment>()
+class MovieAdapter(
+    fragment: Fragment,
+    private val orientation: Int
+) : FragmentStateAdapter(fragment) {
+    private val fragmentList = mutableListOf<MovieInfoFragment>()
 
     override fun getItemCount(): Int = fragmentList.size
 
     override fun createFragment(position: Int): Fragment = fragmentList[position]
+
+    fun submitList(list: List<MovieInfo>) {
+        fragmentList.clear()
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // 세로 모드인 경우
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // 가로 모드인 경우
+        }
+        notifyItemRangeInserted(0, fragmentList.size)
+    }
 }
--- app/src/main/java/com/example/movie/MovieFragment.kt
+++ app/src/main/java/com/example/movie/MovieFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.example.movie.databinding.FragmentMovieBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieFragment : Fragment() {
     private lateinit var binding: FragmentMovieBinding
-    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var adapter: MovieAdapter
+    private val viewModel: MovieViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +29,21 @@ class MovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        movieAdapter = MovieAdapter(this)
-        binding.vpMovieInfo.adapter = movieAdapter
+        viewModel.loadData()
+
+        adapter = MovieAdapter(this, resources.configuration.orientation)
+        binding.vpMovieInfo.apply {
+            adapter = adapter
+            offscreenPageLimit = 1
+            clipChildren = false
+            setPageTransformer(MarginPageTransformer(30.dpToPx(resources.displayMetrics)))
+            (getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+        }
         binding.indicator.setViewPager(binding.vpMovieInfo)
+
+        viewModel.movieList.observe(viewLifecycleOwner) { movieList ->
+            adapter.submitList(movieList)
+            binding.indicator.createIndicators(adapter.itemCount, 0)
+        }
     }
 }
